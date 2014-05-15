771d3bcedc67a641bc3e5cb24518a80c49e32f79
CodeAnimator <stevenc2009@gmail.com>
2014-05-15 16:45:56 +0000

    Worked on room initialization, need to implement Room class

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,21 +1,25 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Dungeon {
 private String[][] dungeon;
 private int numOfRooms;
-private ArrayList<Integer> widthRoom = new ArrayList<Integer>();
-private ArrayList<Integer> lengthRoom = new ArrayList<Integer>();
+//private ArrayList<Integer> widthRoom = new ArrayList<Integer>();
+//private ArrayList<Integer> lengthRoom = new ArrayList<Integer>();
 	public Dungeon (int width, int length){
 		
 		
 		generateDungeon();
 		dungeon = new String[length][width]; //length is number of rows, width is the number of cols
+		for (int a = 0; a < dungeon.length; a++)
+			for (int b = 0; b < dungeon[0].length; b++)
+				dungeon[a][b] = "w";
 	}
 	
 	private void generateDungeon(){
 	initRooms();
-	placeRooms();
+//	placeRoom();
 	initCorridors();
 	placeCorridors();
 	}
@@ -24,8 +28,13 @@ private ArrayList<Integer> lengthRoom = new ArrayList<Integer>();
 		
 	}
 
-	private void placeRooms() {
-		
+	private void placeRoom(int width, int length) {
+		Random rand = new Random();
+		int x = rand.nextInt(dungeon[0].length - width);
+		int y = rand.nextInt(dungeon.length - length);
+		for (int a = x; a < width; a++)
+			for (int b = y; b < length; b++)
+				dungeon[b][a] = "e";
 	}
 
 	private void placeCorridors() {
@@ -35,10 +44,11 @@ private ArrayList<Integer> lengthRoom = new ArrayList<Integer>();
 	private void initRooms() {
 
 		int a = dungeon.length*dungeon[0].length;
-		numOfRooms = (int) ((a/10)+.5);
+		numOfRooms = a/10;
 		for(int b=0; b<numOfRooms;b++){
 			int hi = (int) (Math.random()*3+6); //width
 			int bye = (int) (Math.random()*3+6); //length
+			placeRoom(hi, bye);
 
 		}
 	}
