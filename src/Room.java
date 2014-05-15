c9c2b7c00ba85e5cc82d49a9f333d6b1c653c539
CodeAnimator <stevenc2009@gmail.com>
2014-05-15 16:51:09 +0000

    Room variables updated

--- src/Room.java
+++ src/Room.java
@@ -1,11 +1,13 @@
-
 public class Room {
 private int x;
 private int y;
 private int width;
 private int length;
 	public Room (int x,int y,int width,int length){
-		
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.length = length;
 	}
 	
 }
