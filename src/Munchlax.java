28d851bd98f4d1bab27845ab833751cd84ca562d
Dustboonies <dtfeldsine@gmail.com>
2014-05-20 16:31:01 +0000

    munchlax stats added

--- src/Munchlax.java
+++ src/Munchlax.java
@@ -1,4 +1,11 @@
-
 public class Munchlax extends Pokemon {
+private int level = 1;
+private int hp = 16;
+private int attack = 1;
+private int defense = 3;
+	public Munchlax(){
+	super (level, hp, attack, defense);
+	
+	}
 
 }
