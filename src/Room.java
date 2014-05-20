6a2a93dddbbf27462a1360c44bc4df44d3af1688
Dustboonies <dtfeldsine@gmail.com>
2014-05-20 16:54:19 +0000

    room overlap method completed

--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,5 @@
+import info.gridworld.*;
+
 public class Room {
 private int x;
 private int y;
@@ -14,10 +16,19 @@ private int length;
 		int[] d = new int[]{x, y, width, length};
 		return d;
 	}
-	
-	public boolean overlap(){
-	
+	//clear radius of 5
+	public boolean overlap(Grid d){
+//	Grid g = getGrid();
+	int[] dimensions = getDimensions();
+		for ( int a = x-5; a < x+width+5; a++){
+			for ( int b = y-5; b < y + length + 5; y++){
+				if (a > -1 && a < d.getNumCols() && b > -1 && b > d.getNumRows()){
+					Location l = new Location (a,b);
+					if (g.get(l) instanceof Room)
+						return true;
+				}
+			}
+		}	
+	return false;
 	}
-	
-	
 }
