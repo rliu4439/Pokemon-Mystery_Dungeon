d27049527d46d8a05606b265f42fe4765c642272
Dustboonies <dtfeldsine@gmail.com>
2014-05-20 16:38:14 +0000

    pikachu stats started, not finished

--- src/Pikachu.java
+++ src/Pikachu.java
@@ -1,4 +1,9 @@
-
 public class Pikachu extends Pokemon{
-
+//private int level = 20;
+//private int hp = 50;
+//private int attack = 25
+//private int defense = 21;
+//	public Pikachu(){
+//	super (level, hp, attack, defense);
+//	}
 }
