33fc51cb4ee7e4250abdea462a2f19513e94a2c7
Dustboonies <dtfeldsine@gmail.com>
2014-05-20 16:26:42 +0000

    mudkip stats added

--- src/Mudkip.java
+++ src/Mudkip.java
@@ -1,4 +1,9 @@
-
 public class Mudkip extends Pokemon{
-
+private int level = 1;
+private int hp = 14;
+private int attack = 2;
+private int defense = 3;
+	public Mudkip(){
+		super (level, hp, attack, defense);
+	}
 }
