7f01ac578be3380b60926c55841b39b7664ba729
Ryan L <rsrsdogy@gmail.com>
2014-06-12 22:07:07 -0700

    fixed cyndaquil attack error

--- src/Cyndaquil.java
+++ src/Cyndaquil.java
@@ -9,35 +9,29 @@ import javax.imageio.ImageIO;
 
 public class Cyndaquil extends Pokemon {
 	// private int level = 1;
-	// private int hp = 12;
-	// private int attack = 3;
-	// private int defense = 2;
+	// private int hp = 16;
+	// private int topHp=16; // max hp at current level
+	// private int attack = 1;
+	// private int defense = 3;
 	private static Image front;
 	private static Image back;
 	private static Image left;
 	private static Image right;
 	private Image currentImage;
-	private Image attack;
-
-	public Cyndaquil(boolean enemy, String[][] land, GamePanel panel) {
-		super(enemy, 12, 3, 2, 1, land, "Cyndaquil", panel);
+	private static Image attack;
+	public Cyndaquil(boolean enemy,String[][] land, GamePanel panel) {
+		super(enemy, 16, 1, 3, 1,land, "Cyndaquil", panel);
 		getImages();
 	}
 
-	public void getImages() {
-		if (front == null || back == null || left == null || right == null) {
+	public void getImages(){
+		if (front==null || back==null|| left==null|| right==null){
 			try {
-				back = ImageIO.read(new File(
-						"src/image/cyndaquil/cyndaquil-back.png"));
-				front = ImageIO.read(new File(
-						"src/image/cyndaquil/cyndaquil-front.png"));
-				left = ImageIO.read(new File(
-						"src/image/cyndaquil/cyndaquil-left.png"));
-				right = ImageIO.read(new File(
-						"src/image/cyndaquil/cyndaquil-right.png"));
-				attack = ImageIO
-						.read(new File("src/image/cyndaquil/attack.png"));
-
+				back = ImageIO.read(new File("src/image/cyndaquil/cyndaquil-back.png"));
+				front = ImageIO.read(new File("src/image/cyndaquil/cyndaquil-front.png"));
+				left = ImageIO.read(new File("src/image/cyndaquil/cyndaquil-left.png"));
+				right = ImageIO.read(new File("src/image/cyndaquil/cyndaquil-right.png"));
+				attack = ImageIO.read(new File("src/image/cyndaquil/attack.png"));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
